280c5b7a3c2054a86c01f85741819dbefdff8a86
FlorianTimm <FlorianTimm@github.com>
2025-03-16 23:15:11 +0100

    webhook

---
+++ webhook_view.php
@@ -0,0 +1,20 @@
+<?PHP
+require 'login.php';
+
+$url = 'https://api.strava.com/api/v3/push_subscriptions';
+$data = array(
+    'client_id' => Config::$strava_client,
+    'client_secret' => Config::$strava_secret
+);
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'GET',
+        'content' => http_build_query($data),
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+var_dump($result);
+?>
